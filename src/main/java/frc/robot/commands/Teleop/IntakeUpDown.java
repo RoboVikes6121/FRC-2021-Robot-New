d9abc169b67ff0e21bade7c72660c65da5e5b0c7
Daniel J Latusek <43707815+dlatusek@users.noreply.github.com>
2021-02-18 18:00:43 -0500

    working intake system

--- src/main/java/frc/robot/commands/Teleop/IntakeUpDown.java
+++ src/main/java/frc/robot/commands/Teleop/IntakeUpDown.java
@@ -18,12 +18,14 @@ public class IntakeUpDown extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_Intake.sol();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_Intake.sol();
+    
   }
 
   // Called once the command ends or is interrupted.
