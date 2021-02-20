2405ef34333ccf947cbb60eeb62f5e92412f6243
Daniel J Latusek <43707815+dlatusek@users.noreply.github.com>
2021-02-20 09:27:58 -0500

    Not Yet Working Shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -30,14 +30,17 @@ public class Shooter extends SubsystemBase {
     // This method will be called once per scheduler run
   }
   public void end(){ //off
+    System.out.println("OFF");
     shooter.set(0);
     elevator.set(0);
   }
   public void start(){ //on
+    System.out.println("ON");
     shooter.set(0.75);
     elevator.set(0.75);
   }
   public void shoot(){ //loads ball into elevator
+    System.out.println("SHOOT");
     kicker.set(DoubleSolenoid.Value.kForward);
     Timer.delay(1.0);
     kicker.set(DoubleSolenoid.Value.kReverse);
