d93cdbc7c354c1778c22c7bd1e1e40976f74a94d
Daniel J Latusek <43707815+dlatusek@users.noreply.github.com>
2021-02-24 16:10:56 -0500

    Step 3

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
+  /** Creates a new Intake. */
+  private static WPI_VictorSPX intake = new WPI_VictorSPX(Constants.INTAKE);
+  private static DoubleSolenoid intakeDoubleSolenoid = new DoubleSolenoid(Constants.INTAKE1,Constants.INTAKE2);
+  public Intake() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void start(){
+    intake.set(.75);
+  }
+
+  public void end(){
+    intake.set(0);
+    System.out.println("COMMAND ENDED");
+  }
+
+  public void sol() {
+    if(intakeDoubleSolenoid.get().equals(DoubleSolenoid.Value.kReverse)){
+      intakeDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
+      System.out.println("DOWN");
+    }else{
+      intakeDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+      System.out.println("UP");
+    }
+  }
+}
